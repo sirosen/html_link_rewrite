b9086b5dae8332e7c7e2950bc461d7e44a22c8c0
Stephen Rosen <sirosen@uchicago.edu>
2013-10-29 14:16:43 -0500

    Added the actual link rewriting code.
    Also traverses the text of nodes and rewrites any instances of the old
    links with the new ones. By default, names everything by its previous
    absolute path prepended with '/tmp'.

--- rewrite_file.py
+++ rewrite_file.py
@@ -2,10 +2,17 @@
 
 from __future__ import print_function
 
-import sys, lxml.html
-from os.path import join as pjoin
+import sys, lxml.html, os
+from os.path import join as pjoin, dirname, exists, abspath
 
 import json_parse
+
+def etree_text_replace(node,mappings):
+    if node.text:
+        for old,new in mappings.iteritems():
+            node.text = node.text.replace(old,new)
+    for child in node:
+        etree_text_replace(child,mappings)
 
 def rewrite_and_save(filename,mappings=None,new_filename=None):
     parsed_html = None
@@ -14,10 +21,28 @@ def rewrite_and_save(filename,mappings=None,new_filename=None):
     if not parsed_html:
         raise IOError("Could not read file " + filename)
     if not new_filename:
-        new_filename = pjoin('/tmp/',filename)
+        new_filename = pjoin('/tmp',abspath(filename)[1:])
     if not mappings:
         mappings = json_parse.parse('link_dictionary.json')
-    print('STUB!')
+
+    def link_repl(oldlink):
+        try:
+            return mappings[oldlink]
+        except:
+            return oldlink
+
+    root = parsed_html.getroot()
+    root.rewrite_links(link_repl)
+    etree_text_replace(root,mappings)
+
+    # Race condition out the wazoo here, but it's not worth the effort
+    # of making this more robust
+    directory = dirname(new_filename)
+    if not exists(directory):
+        os.makedirs(directory)
+
+    with open(new_filename,'w+') as f:
+        print(lxml.html.tostring(parsed_html),file=f)
 
 if __name__ == '__main__':
     rewrite_and_save(sys.argv[1])
